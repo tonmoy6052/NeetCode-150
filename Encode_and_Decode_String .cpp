2c3094b1b5a8f0c47891087d6e624d4c6f55c5ea
tonmoy6052 <140625730+tonmoy6052@users.noreply.github.com>
2025-09-03 00:12:19 +0600

    Update Encode_and_Decode_String .cpp

--- Encode_and_Decode_String .cpp
+++ Encode_and_Decode_String .cpp
@@ -3,6 +3,8 @@ public:
 
     string encode(vector<string>& strs) {
         string ans="";
+        //edge case
+        if(strs.size()==0)return ans;
         for(auto x:strs){
             ans.push_back('.');
             ans+=x;
@@ -13,6 +15,15 @@ public:
     vector<string> decode(string s) {
         vector<string>ans;
         string p="";
+        //edge case
+        if(s.size()==0){
+            return ans;
+        }
+        //edge case
+        if(s.size()==1){
+            ans.push_back(p);
+            return ans;
+        }
         for(int i=1;i<s.size();i++){
             if(s[i]=='.'){
                 ans.push_back(p);

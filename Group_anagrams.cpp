ada920d72be391bbf9e54b47a320a4c442f84532
tonmoy6052 <140625730+tonmoy6052@users.noreply.github.com>
2025-09-02 23:48:33 +0600

    Create Group_anagrams.cpp

---
+++ Group_anagrams.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>ans;
+        map<string,vector<string>>m;
+        for(auto x:strs){
+            auto y=x;
+            sort(y.begin(),y.end());
+            m[y].push_back(x);
+        }
+        for(auto x:m){
+            ans.push_back(x.second);
+        }
+        return ans;
+    }
+};
